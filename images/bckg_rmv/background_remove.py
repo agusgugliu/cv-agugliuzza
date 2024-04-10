ddbb03462d81a95d26a990faf48807ce9366cb1d
Agustin Gugliuzza <agugliuzzapicci@gmail.com>
2024-04-10 17:12:57 -0300

    modified images and style

--- images/bckg_rmv/background_remove.py
+++ images/bckg_rmv/background_remove.py
@@ -1,12 +1,13 @@
 from rembg import remove
 from PIL import Image
 
+x_folder = input('Image Folder:\t')
 x_image = input('Image Name:\t')
 x_format = input('Image Format:\t')
 
-input_path = "C:\\Users\\Usr\\Documents\\GitHub\\cv-agugliuzza\\images\\certificates\\" + x_image + '.' + x_format
+input_path = "C:\\Users\\Usr\\Documents\\GitHub\\cv-agugliuzza\\images\\" + x_folder + '\\' + x_image + '.' + x_format
 
-output_path = "C:\\Users\\Usr\\Documents\\GitHub\\cv-agugliuzza\\images\\certificates\\" + x_image + '_bck.png'
+output_path = "C:\\Users\\Usr\\Documents\\GitHub\\cv-agugliuzza\\images\\" + x_folder + '\\' + x_image + '_bck.png'
 
 input = Image.open(input_path)
 output = remove(input)
