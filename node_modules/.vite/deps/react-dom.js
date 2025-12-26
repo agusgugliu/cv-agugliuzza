29d5b1f60503ec762b577b317d234bbdd9e7e330
Agustín Gugliuzza <agusgugliuzza@gmail.com>
2025-12-26 10:08:55 -0300

    chore: Add project dependencies.

---
+++ node_modules/.vite/deps/react-dom.js
@@ -0,0 +1,6 @@
+import {
+  require_react_dom
+} from "./chunk-YF4B4G2L.js";
+import "./chunk-WUR7D6NS.js";
+import "./chunk-G3PMV62Z.js";
+export default require_react_dom();
---
+++ node_modules/.vite/deps/react.js
@@ -0,0 +1,5 @@
+import {
+  require_react
+} from "./chunk-WUR7D6NS.js";
+import "./chunk-G3PMV62Z.js";
+export default require_react();
